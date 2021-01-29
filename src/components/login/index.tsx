395ec101299d7205323a04bd55e84c22101aa44b
André Loureiro <loureiro.andrew@gmail.com>
2021-01-28 23:03:24 -0400

    [Feat] Add a react.ref to navigation container
    - Also removes the default header title added by stack navigator
      avoiding it to appear over the tabs screens.
    
    Signed-off-by: André Loureiro <loureiro.andrew@gmail.com>

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -3,6 +3,8 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator, TransitionPresets} from '@react-navigation/stack';
 import LoginForm from './LoginForm';
 import ForgotPassword from './ForgotPassword';
+import HomeNavigator from '../home';
+import * as RootNavigation from '../../navigation';
 
 const TransitionsScreenOptions = {
   ...TransitionPresets.SlideFromRightIOS,
@@ -11,18 +13,26 @@ const RootStack = createStackNavigator();
 
 const RootNavigator: React.FC = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer ref={RootNavigation.navigationRef}>
       <RootStack.Navigator
         initialRouteName="Login"
-        screenOptions={TransitionsScreenOptions}>
+        screenOptions={TransitionsScreenOptions}
+        headerMode="none">
         <RootStack.Screen
           name="Login"
           component={LoginForm}
           options={{
-            headerShown: false, // change this to `false`
+            headerShown: false,
           }}
         />
         <RootStack.Screen name="ForgotPassword" component={ForgotPassword} />
+        <RootStack.Screen
+          name="Home"
+          component={HomeNavigator}
+          options={{
+            headerShown: false,
+          }}
+        />
       </RootStack.Navigator>
     </NavigationContainer>
   );

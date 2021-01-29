c8682e8768910b43db7ef7144a9525f7ef74ecec
André Loureiro <loureiro.andrew@gmail.com>
2021-01-28 23:01:30 -0400

    [Feat] Add navigation ref to be referenced by navigation container
    Signed-off-by: André Loureiro <loureiro.andrew@gmail.com>

---
+++ src/navigation/index.ts
@@ -0,0 +1,27 @@
+import React from 'react';
+import {NavigationContainerRef, RouteProp} from '@react-navigation/native';
+import {BottomTabNavigationProp} from '@react-navigation/bottom-tabs';
+import {UserData} from '../actions';
+
+export type RootTabParamsList = {
+  Perfil: {user: UserData};
+  Home: {user: UserData};
+  Compras: {user: UserData};
+  Limites: {user: UserData};
+};
+export type HomeUserRouteProp = RouteProp<RootTabParamsList, 'Home'>;
+export type HomeUserNavigationProp = BottomTabNavigationProp<
+  RootTabParamsList,
+  'Home'
+>;
+
+export type HomeUserProps = {
+  route: HomeUserRouteProp;
+  navigation: HomeUserNavigationProp;
+};
+
+export const navigationRef = React.createRef<NavigationContainerRef>();
+
+export function navigate(name: string, params: any) {
+  navigationRef.current?.navigate(name, params);
+}
